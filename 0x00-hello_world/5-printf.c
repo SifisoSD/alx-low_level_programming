4ac7f162b32339cac0b43cb8433d9b371ed2f2bc
SifisoSD <sifisodlulisasd@gmail.com>
2022-09-12 18:37:52 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - print a text
+* main - print the string in the put function
 *
-*Return: 0 (success)
+* Description using the main function
+* Return:0
 */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\");
 	return (0);
 }
