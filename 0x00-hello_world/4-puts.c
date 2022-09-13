d776dcbb95e1ce9bc8cfe6ff296e9936db8a1aa1
SifisoSD <sifisodlulisasd@gmail.com>
2022-09-12 17:49:55 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>                                                                                                                                                                                      
+
+                                                                                                                                                                                                        
+
+/**                                                                                                                                                                                                     
+
+ *main - Printing                                                                                                                                                                                       
+
+ *                                                                                                                                                                                                      
+
+ *Return: Always 0 (Successful)                                                                                                                                                                         
+
+ */                                                                                                                                                                                                     
+
+int main(void)                                                                                                                                                                                          
+
+{                                                                                                                                                                                                       
+
+        puts("\"Programming is like building a mulitilingual puzzle");                                                                                                                                  
+
+        return(0);
+}
