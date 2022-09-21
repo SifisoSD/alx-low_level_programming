e965fe06fe3f0f7f3fb5af34d0b78c092aa5d348
SifisoSD <sifisodlulisasd@gmail.com>
2022-09-21 06:05:20 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,28 +8,6 @@
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-	printf("Size of char: %ld byte(s)\n", sizeof(c));
-	printf("Size of int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
-#include <stdio.h>
-
-/**
-* main - print the string in the put function
-* Description: using the main function
-* this program prints "Programming is like building a multiligual puzzle"
-* Return: 0
-*/
-int main(void)
-{
         char c;
         int i;
         long li;
@@ -38,7 +16,7 @@ int main(void)
 
         printf("Size of char: %ld byte(s)\n", sizeof(c));
         printf("Size of int: %ld byte(s)\n", sizeof(i));
-       printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+        printf("Size of a long int: %ld byte(s)\n", sizeof(li));
         printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
         printf("Size of a float: %ld byte(s)\n", sizeof(f));
         return (0);
