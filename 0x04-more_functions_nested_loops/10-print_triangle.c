e2503de12c3b45e3fd5c41eeaeeb2d5787429773
SifisoSD <111223973+SifisoSD@users.noreply.github.com>
2022-09-23 20:52:25 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -24,64 +24,6 @@ void print_triangle(int size)
 			{
 				putchar('#');
 			}
-			putchar('#');
-#include <stdio.h>
-#include "main.h"
-
-/**
-*print_triangle - prints a triangle
-*@size:size parameter of triangle
-*Return: returns nothing
-*/
-
-void print_triangle(int size)
-{
-	int inc1, inc2;
-
-	if (size > 0)
-	{
-		for (inc1 = 1; inc1 <= size; inc1++)
-		{
-			putchar(' ');
-		}
-
-		for (inc2 = 0; inc2 < inc1; inc2++)
-		{
-			putchar('#');
-		}
-		if (inc1 == size)
-		{
-			continue;
-		}
-
-		putchar('\n');
-	}
-#include <stdio.h>
-#include "main.h"
-
-/**
-*print_triangle - prints a triangle
-*@size:size parameter of triangle
-*Return: returns nothing
-*/
-
-void print_triangle(int size)
-{
-	int inc1, inc2;
-
-	if (size > 0)
-	{
-		for (inc1 = 1; inc1 <= size; inc1++)
-		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
-			{
-				putchar(' ');
-			}
-
-			for (inc2 = 0; inc2 < inc1; inc2++)
-			{
-				putchar('#');
-			}
 
 			if (inc1 == size)
 			{
@@ -90,6 +32,6 @@ void print_triangle(int size)
 
 			putchar('\n');
 		}
-	]
+	}
 	putchar('\n');
 }
